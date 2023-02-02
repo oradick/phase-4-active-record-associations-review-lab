a1ce5abd11d248d2b6c739e57059c918b2d1f9a3
Olivia Radick <olivia.radick@gmail.com>
2023-02-02 17:07:40 -0500

    commit tree 0b77b8882228c8423844a64cfdaa04e0c6d501bb

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,9 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.references :taxi
+      t.references :passenger
+      
       t.timestamps
     end
   end
